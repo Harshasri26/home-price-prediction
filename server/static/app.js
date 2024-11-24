fee12e720229ff7426267db807e5f76395c3468b
Harshasri26 <harshasri.work@gmail.com>
2024-11-24 20:42:52 +0530

    Add project report

--- server/static/app.js
+++ server/static/app.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-    // Fetch location names from the Flask API
     $.get("/api/location_names", function(data) {
         if (data.locations && data.locations.length > 0) {
-            // Populate the dropdown with location options
             data.locations.forEach(function(location) {
                 $('#uiLocations').append('<option value="' + location + '">' + location + '</option>');
             });
@@ -10,25 +8,18 @@ $(document).ready(function() {
             console.log("No locations found.");
         }
     });
-
-    // Event handler for the 'Estimate Price' button click
     $('#estimatePriceButton').on('click', function() {
         var total_sqft = $('#uiSqft').val();
         var location = $('#uiLocations').val();
         var bhk = $("input[name='uiBHK']:checked").val();
         var bath = $("input[name='uiBathrooms']:checked").val();
-
-        // Display a loading message until we get the result
         $('#uiEstimatedPrice span').text('Loading...');
-
-        // Call API to get the predicted price
         $.post("/api/predict_home_price", {
             total_sqft: total_sqft,
             location: location,
             bhk: bhk,
             bath: bath
         }, function(response) {
-            // After receiving the response, update the price
             if(response && response.estimated_price) {
                 $('#uiEstimatedPrice span').text('₹' + response.estimated_price);
             } else {
